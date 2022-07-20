7ae3a0ec0b0207b194d37b91b5dc9cb984806290
Amanullah21 <1amanpce@gmail.com>
2022-07-20 14:10:34 +0530

    compeletly Map feature add to UI whenever click on map logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Location from "./component/Location";
 import Ui from "./component/Ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
